64d4582823965388335aceb793404779d298a135
Victor Cajuba de Britto Bacelar Caldas <victorcajuba@gmail.com>
2016-10-31 21:52:19 -0300

    funcao pop

---
+++ ex17.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 100
+/* Aqui vão as funcoes para fazer a pilha */
+
+typedef struct st_pilha
+{
+    char car;
+    struct st_pilha *prox;
+} pilha;
+
+void push(pilha **cabeca, char c)
+{
+    pilha *pp=*cabeca;
+    pp=malloc(sizeof(pilha));
+    pp->car=c;
+    pp->prox=NULL;
+    if(cabeca == NULL)
+    {
+        pp->prox=NULL;
+        *cabeca=pp;
+    }
+    else
+    {        
+        pp->prox=*cabeca;
+        *cabeca=pp;
+    }
+    return;
+}
+
