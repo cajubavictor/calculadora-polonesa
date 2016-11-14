4148990f15606d248f13da4b1c068d4ee5145c2b
Victor Cajuba de Britto Bacelar Caldas <victorcajuba@gmail.com>
2016-11-13 21:09:13 -0300

    conserto de bug do char e variaveis inuteis apagadas

--- ex17.c
+++ ex17.c
@@ -18,41 +18,33 @@ int main(void)
 {
 
     pilha *head=NULL, *head2=NULL;
-    char   saida,n1,n2,sinal,result='0';
-    char x,resultInt=0;
+    char n1,n2,sinal,result='0';
     char ent;
 
-    while(ent != 'a')
+    printf("Digite a equacao na forma polonesa.: ");
+    while((ent =getchar())!='\n' )
     {
-        scanf("%c",&ent);
-        if(ent != '\n' && ent != 'a')
+        if( ent == '+' || ent == '-' || ent  == 'x'|| ent  == '/')
         {
-            if( ent == '+' || ent == '-' || ent  == 'x'|| ent  == '/')
-            {
-                push(&head2,ent);
-                n2=pop(&head);
-                n1=pop(&head);
-                result = operacao(n1-'0',ent,n2-'0');
-                push(&head2,result);
-                printf("resuuuuuuuuuuuuuult  %d ",result);
+            n2=pop(&head);
+            n1=pop(&head);
+            result = operacao(n1-'0',ent,n2-'0');
+            push(&head,result+'0');
 
-            }
-            else
-                push(&head,ent);
         }
+        else
+            push(&head,ent);
     }
+    result=pop(&head);
+    printf("Resultado %d\n",result - '0');
     return 0;
 }
 char operacao(char   n1,char operador,char n2)
 {
     char    resultado ;
 
-    printf("n1= %d \n n2 = %d\n",n1,n2);
     if('+'== operador)
-    {
-        printf("aaaaaaaaaaaaaaaaa\n");
         resultado = n1 + n2;
-    }
     if('-' == operador)
         resultado = n1 - n2;
     if('x' == operador || '*'== operador)
